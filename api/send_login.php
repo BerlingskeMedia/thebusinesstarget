54ee6e3eb2b48f62fdb02831ab39e9b67ca60a7a
Marcus Gregersen <mgmano@gmail.com>
2015-11-11 20:29:45 +0100

    Login endpoint

--- api/send_login.php
+++ api/send_login.php
@@ -1,11 +1,16 @@
 <?php
-  //Get ID by email
-  //Send email with ekstern ID
+  $config = include_once("config.php");
+  require_once 'Requests/library/Requests.php';
+  Requests::register_autoloader();
+  $profile_page_link_url = $config['BASEURL'] . '/mails/profile-page-link';
   $postdata = file_get_contents("php://input");
   $request = json_decode($postdata);
   $mail = $request->mail;
-  $oneshot_url_format = "http://apps.adlifter.com/plugin/?application_key=70c4dd8af56f5dad9d5483513f049eb6&customer_key=a40e4703a823d1d11977928620c6ead0&stack_key=6ff47cb3441841ac37fd1bca752d6bff&command=cmd_send_oneshot&email=%s&mail_data_id=3107&mail_id=13367";
-  $oneshot_url = sprintf($oneshot_url_format, $mail);
-  file_get_contents($oneshot_url);
-  echo (json_encode(""));
+  $payload = new stdClass();
+  $payload->publisher_id = 51;
+  $payload->email = $mail;
+  $headers = array('Content-Type' => 'application/json');
+  $response = Requests::post($profile_page_link_url, $headers, json_encode($payload));
+  echo $response->body;
+
 ?>
