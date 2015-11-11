36a938b99631905827156945ddd349aee63cad20
Marcus Gregersen <mgmano@gmail.com>
2015-11-11 20:29:29 +0100

    semicolon

--- api/config.php
+++ api/config.php
@@ -2,5 +2,5 @@
   ini_set('display_errors', 1);
   ini_set('display_startup_errors', 1);
   error_reporting(E_ALL);
-  return array('BASEURL' => 'http://54.77.4.249:8080')
+  return array('BASEURL' => 'http://54.77.4.249:8080');
 ?>
