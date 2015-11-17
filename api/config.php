ffc44288893c0007defccaa389d535cc4126a9da
Marcus Gregersen <mgmano@gmail.com>
2015-11-17 09:15:26 +0100

    Remove debug flags

--- api/config.php
+++ api/config.php
@@ -1,6 +1,3 @@
 <?php
-  ini_set('display_errors', 1);
-  ini_set('display_startup_errors', 1);
-  error_reporting(E_ALL);
   $config = array('BASEURL' => 'http://54.77.4.249:8080', 'LOCATION_ID' => 1, 'TBT_NID' => "844", 'TBT_PUBLISHER_ID' => 51);
 ?>
